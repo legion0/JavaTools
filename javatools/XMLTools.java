1ef749f83ef8049103d722464856a6819e4de645
Kibarski Jonatan <phenixdoc@gmail.com>
2013-03-18 19:09:03 +0200

    Initial commit

---
+++ javatools/XMLTools.java
@@ -0,0 +1,55 @@
+package javatools;
+
+import java.io.OutputStream;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.bootstrap.DOMImplementationRegistry;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSOutput;
+import org.w3c.dom.ls.LSSerializer;
+
+public class XMLTools {
+
+	public static void prettyPrintXML(Document doc, OutputStream out) {
+		try {
+			DOMImplementationRegistry registry = DOMImplementationRegistry.newInstance();
+			DOMImplementationLS impl = (DOMImplementationLS)registry.getDOMImplementation("LS");
+			LSSerializer writer = impl.createLSSerializer();
+			writer.getDomConfig().setParameter("format-pretty-print", Boolean.TRUE);
+			LSOutput output = impl.createLSOutput();
+			output.setEncoding("UTF-8");
+			output.setByteStream(out);
+			writer.write(doc, output);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void prettyPrintXML2(Document doc, OutputStream out) {
+		try {
+			TransformerFactory transfac = TransformerFactory.newInstance();
+			transfac.setAttribute("indent-number", new Integer(4));
+			Transformer trans = transfac.newTransformer();
+			trans.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+			trans.setOutputProperty(OutputKeys.STANDALONE, "no");
+			trans.setOutputProperty(OutputKeys.INDENT, "yes");
+			trans.transform(new DOMSource(doc), new StreamResult(out));
+		} catch (TransformerConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
